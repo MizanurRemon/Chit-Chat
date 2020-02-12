e02ce0cbe932112133b1fdcb7d9b9ef25d876760
Remon <mr248903@gmail.com>
2020-02-13 01:49:09 +0600

    third commit

---
+++ Chit Chat/Controller/signUp.swift
@@ -0,0 +1,30 @@
+//
+//  signUp.swift
+//  Chit Chat
+//
+//  Created by Mizanur Remon on 13/2/20.
+//  Copyright © 2020 Mizanur Remon. All rights reserved.
+//
+
+import UIKit
+
+class signUp: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Chit Chat/View/signIn.swift
@@ -0,0 +1,30 @@
+//
+//  signIn.swift
+//  Chit Chat
+//
+//  Created by Mizanur Remon on 13/2/20.
+//  Copyright © 2020 Mizanur Remon. All rights reserved.
+//
+
+import UIKit
+
+class signIn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
