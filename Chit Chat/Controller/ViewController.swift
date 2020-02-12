8e7f3b0b51a6326df916f590060e19a1e1e86725
Remon <mr248903@gmail.com>
2020-02-13 01:20:17 +0600

    second commit

--- Chit Chat/Controller/ViewController.swift
+++ Chit Chat/Controller/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var signUpoutlet: UIButton!
+    
+    @IBOutlet weak var signInoutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        signUpoutlet.layer.cornerRadius = 6
+        signInoutlet.layer.cornerRadius = 6
     }
 
+    @IBAction func exit(_ sender: Any) {
+        let alert = UIAlertController(title: "Quit!", message: "Are you sure?", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        
+        alert.addAction(UIAlertAction(title: "Yes", style: .destructive, handler: { _ in
+        UIControl().sendAction(#selector(NSXPCConnection.suspend), to: UIApplication.shared, for: nil)}))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
